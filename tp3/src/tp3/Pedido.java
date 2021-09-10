d67d2c349b0499cc430dfc0522237ec7c3e0a01c
Eruel6 <arthur_taylor2001@hotmail.com>
2021-09-10 00:45:45 -0300

    Adição de Arraylist
    Foi adicionado um arraylist para itens da classe pedido, tendo em vista
    que um pedido poderá ter mais de um item.

--- tp3/src/tp3/Pedido.java
+++ tp3/src/tp3/Pedido.java
@@ -1,23 +1,22 @@
 package tp3;
 
+import java.util.ArrayList;
+
 public class Pedido {
 
 	private int numero;
-	private String itens;
+	static private ArrayList<Comida> itens;
 	private int subtotal;
 	private String metodoDePagamento;
 	private Boolean retirada;
 	
-	public Pedido(int numero, String itens, int subtotal, String metodoDePagamento, Boolean retirada) {
-		super();
+	public Pedido(int numero, ArrayList<Comida> itens, int subtotal, String metodoDePagamento, Boolean retirada) {
 		this.numero = numero;
-		this.itens = itens;
+		itens = new ArrayList<Comida>();
 		this.subtotal = subtotal;
 		this.metodoDePagamento = metodoDePagamento;
 		this.retirada = retirada;
 	}
-	
-	
 	
 
 	@Override
@@ -37,11 +36,11 @@ public class Pedido {
 		this.numero = numero;
 	}
 
-	public String getItens() {
+	public ArrayList<Comida> getItens() {
 		return itens;
 	}
 
-	public void setItens(String itens) {
+	public void setItens(ArrayList<Comida> itens) {
 		this.itens = itens;
 	}
 
